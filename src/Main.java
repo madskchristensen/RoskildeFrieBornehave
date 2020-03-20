ba2644d1fd7986d376f6b567948fecff4f22a336
Mads <maller2650@gmail.com>
2020-03-20 11:59:03 +0100

    uploaded POJOs + repositories + basic Login.fxml & Login.controller

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static Stage window;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        window = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
