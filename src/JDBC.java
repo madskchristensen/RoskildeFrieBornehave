5e714cd0b63ea74e0d29722f521344cc4bc80ece
Mads <maller2650@gmail.com>
2020-03-20 16:19:57 +0100

    - udvidet JDBC utility

--- src/JDBC.java
+++ src/JDBC.java
@@ -3,28 +3,78 @@ import java.sql.*;
 public class JDBC {
     private String username = "";
     private String password = "";
-    private static final String URL = "jdbc:mysql://localhost/login?serverTimezone=UTC";
+    private Connection con = null;
+    private Statement stmt = null;
+    private static final String URL = "jdbc:mysql://localhost/school?serverTimezone=UTC";
 
-    public JDBC(String username, String password) {
+    public JDBC(String username, String password) throws SQLException {
         this.username = username;
         this.password = password;
+        openConnection();
+        this.stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
     }
     public Connection openConnection() throws SQLException {
-        Connection con = null;
+            this.con = DriverManager.getConnection(URL, username, password);
+        return this.con;
+    }
 
-        try {
-            con = DriverManager.getConnection(URL, username, password);
-        } catch(SQLException e) {
-            e.printStackTrace();
+    public Connection getConnection() {
+        return this.con;
+    }
+
+    public void closeConnection() throws SQLException {
+        this.con.close();
+    }
+
+    public ResultSet select(String table, String column) throws SQLException {
+        return stmt.executeQuery("SELECT " + column + " FROM " + table + ";");
+    }
+
+    public ResultSet select(String table, String column, String where) throws SQLException {
+        return stmt.executeQuery("SELECT " + column + " FROM " + table + " WHERE " + where + ";");
+    }
+
+    public boolean insert(String table, String columns[], String values[]) throws SQLException {
+        String queryColumns = "(";
+
+        for (int i = 0; i < columns.length - 1; i++) {
+            queryColumns += columns[i] + ", ";
         }
-        return con;
-    }
-    public Boolean Insert(String table, String columns[], String values[]){
-        return null;
+        queryColumns += columns[columns.length - 1] + ")";
+
+        String queryValues = "(";
+
+        for (int i = 0; i < values.length - 1; i++) {
+            if(Integer.getInteger(values[i]) == null) {
+                values[i] = "\"" + values[i] + "\"";
+            }
+            queryValues += values[i] + ", ";
+        }
+        if(Integer.getInteger(values[values.length - 1]) == null) {
+            values[values.length - 1] = "\"" + values[values.length - 1] + "\"";
+        }
+        queryValues += values[values.length - 1] + ")";
+
+        stmt.executeUpdate("INSERT INTO " + table + " " + queryColumns + " VALUES " + queryValues);
+
+        return false;
     }
 
-    public void closeConnection(Connection con) throws SQLException {
-        con.close();
+    public boolean update(String table, String columns[], String values[]) {
+        return false;
     }
-    
+
+    public boolean delete(String table, String column) {
+        return false;
+    }
+
+    public void print(ResultSet rs) throws SQLException {
+        while(rs.next()) {
+            for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                System.out.print(rs.getString(i) + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
