e19e523c6667efcd14bd7e61d2d4bbd7ec49d9bb
Rasmussejer <rasm897c@stud.kea.dk>
2020-03-21 11:42:30 +0100

    teacher/pædagog FXML

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,4 +1,13 @@
+import javafx.scene.control.Button;
+
 public class Teacher extends TeacherRepository implements Member {
+
+
+    public Button buttonVagtplan;
+    public Button buttonChild;
+    public Button buttonParent;
+    public Button buttonTeacherLogOut;
+
     private int id;
     private String name;
     private String[] classroom;
@@ -16,5 +25,21 @@ public class Teacher extends TeacherRepository implements Member {
     @Override
     public int getId() {
         return this.id;
+    }
+
+    public boolean handleButtonVagtplan() {
+        return true;
+    }
+
+    public boolean handleButtonChild(){
+        return true;
+    }
+
+    public boolean handleButtonParent(){
+        return true;
+    }
+
+    public boolean handleLogOut(){
+        return true;
     }
 }
