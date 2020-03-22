b92c678aa85eedeabb9da2a0dfc71f8f9130d055
Michael Fuglø <michael@fuglo.com>
2020-03-22 19:08:40 +0100

    Fjernet lidt skrald fra login

--- src/Login.java
+++ src/Login.java
@@ -13,10 +13,7 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
-public class Login {//extends AbstractController implements Initializable {
-    LoginPopUp loginpopUp = new LoginPopUp();
-    @FXML private Label resultLbl;
-    @FXML private Button loginButton;
+public class Login{
 
     public Button buttonTeacherLogin;
     public Button buttonLoginAdmin;
@@ -25,47 +22,4 @@ public class Login {//extends AbstractController implements Initializable {
         System.out.println("logging in");
         return true;
     }
-/*
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        resultLbl.setText("");
-        buttonTeacherLogin.setOnAction((event)->{
-            HashMap<String, Object> resultMap = showPopupWindow();
-            resultLbl.setText("username: "+resultMap.get("username")
-                    +" /Password: "+resultMap.get("password")+")");
-        });
-
-    }
-    private HashMap<String, Object> showPopupWindow() {
-        HashMap<String, Object> resultMap = new HashMap<String, Object>();
-
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("LoginPopUp.fxml"));
-        // initializing the controller
-        LoginPopUp loginPopUp = new LoginPopUp();
-        loader.setController(loginPopUp);
-        Parent layout;
-        try {
-            layout = loader.load();
-            Scene scene = new Scene(layout);
-            // this is the popup stage
-            Stage popupStage = new Stage();
-            // Giving the popup controller access to the popup stage (to allow the controller to close the stage)
-            loginPopUp.setStage(popupStage);
-            if(this.main!=null) {
-                popupStage.initOwner(main.getPrimaryStage());
-            }
-            popupStage.initModality(Modality.WINDOW_MODAL);
-            popupStage.setScene(scene);
-            popupStage.showAndWait();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return loginPopUp.getResult();
-    }
-
-
-}
-*/
 }
