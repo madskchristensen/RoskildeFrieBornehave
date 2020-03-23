ed361f7afde116b1e031c3840a67cfbe697cd81d
Rasmussejer <rasm897c@stud.kea.dk>
2020-03-23 12:23:30 +0100

    Login fix

--- src/Login.java
+++ src/Login.java
@@ -14,17 +14,25 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
-public class Login{
+public class Login {
 
-    public Button buttonTeacherLogin;
-    public Button buttonLoginAdmin;
+    @FXML
+    private Button buttonTeacherLogin;
+    @FXML
+    private Button buttonLoginAdmin;
 
-
-    public void handleLoginTeacher(ActionEvent actionEvent) {
-        System.out.println("logging in");
+    public void handleLoginTeacher(ActionEvent actionEvent) throws IOException {
+        Parent layout = FXMLLoader.load(getClass().getResource("LoginPopUp.fxml"));
+        Scene scene = new Scene(layout);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Roskilde Frie Børnehave");
+        stage.show();
     }
 
     public void handleLoginAdmin(ActionEvent actionEvent) {
         System.out.println("logging in");
     }
+
+
 }
