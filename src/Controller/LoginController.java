3654317d6faa57f5a131670796d45e34aed4bec8
Mads <maller2650@gmail.com>
2020-03-24 16:31:20 +0100

    - lavet knapper til at skippe login imens vi laver programmet

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -13,10 +13,24 @@ public class LoginController {
     private Button buttonTeacherLogin;
     @FXML
     private Button buttonLoginAdmin;
+    @FXML
+    private Button buttonSkipLoginAdmin;
+    @FXML
+    private Button buttonSkipLoginTeacher;
 
     public void handleLogin(ActionEvent actionEvent) throws IOException {
         SceneManager sceneManager = new SceneManager(new Stage());
         sceneManager.setSize(250,200);
         sceneManager.switchScene("LogInPopUp.fxml", "Log in");
+    }
+
+    @FXML
+    private void loginSkipAdmin() throws IOException {
+        Main.sceneManager.switchScene("AdminMenu.fxml", "Teacher page");
+    }
+
+    @FXML
+    private void loginSkipTeacher() throws IOException {
+        Main.sceneManager.switchScene("TeacherMenu.fxml", "Teacher admin page");
     }
 }
