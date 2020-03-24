31395fdfaf06dbd7f68389d77b8def59ba8fff13
Peter Bandsholm <lil.jdean@gmail.com>
2020-03-24 12:21:09 +0100

    message

---
+++ src/AdminChildListController.java
@@ -0,0 +1,44 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class AdminChildListController {
+
+    public javafx.scene.control.TableColumn firstName;
+    public javafx.scene.control.TableColumn lastName;
+    public javafx.scene.control.TableColumn age;
+    public javafx.scene.control.TableColumn classroom;
+    public javafx.scene.control.TableColumn dateOfBirth;
+    public javafx.scene.control.TableColumn address;
+    public Button back;
+    public Button guardian;
+    public Button create;
+    public Button update;
+    public Button delete;
+
+    public void readGuardian(ActionEvent actionEvent) {
+    }
+
+    public void goBack(ActionEvent actionEvent) {
+    }
+
+    public void addToChildList(ActionEvent actionEvent) {
+    }
+
+    public void updateChildList(ActionEvent actionEvent) {
+    }
+
+    public void deleteFromChildList(ActionEvent actionEvent) {
+    }
+
+
+   /* public void initialize() {
+        TableColumn id = new TableColumn("ID");
+        TableColumn firstName = new TableColumn("firstName");
+        TableColumn lastName = new TableColumn("lastName");
+        TableColumn classroom = new TableColumn("class");
+    }
+*/
+
+
+}
