d78e9afe94a77986a1d14a80db9e9436b6b17dbd
Peter Bandsholm <lil.jdean@gmail.com>
2020-03-24 12:38:32 +0100

    message

--- src/AdminChildListController.java
+++ src/AdminChildListController.java
@@ -1,23 +1,28 @@
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminChildListController {
 
+    public javafx.scene.control.TableColumn ID;
     public javafx.scene.control.TableColumn firstName;
     public javafx.scene.control.TableColumn lastName;
-    public javafx.scene.control.TableColumn age;
     public javafx.scene.control.TableColumn classroom;
-    public javafx.scene.control.TableColumn dateOfBirth;
-    public javafx.scene.control.TableColumn address;
     public Button back;
     public Button guardian;
     public Button create;
     public Button update;
     public Button delete;
 
-    public void readGuardian(ActionEvent actionEvent) {
-    }
+    public void readGuardian(ActionEvent actionEvent) throws IOException {
+            SceneManager sceneManager = new SceneManager(new Stage());
+            sceneManager.setSize(250,200);
+            sceneManager.switchScene("PopUp.fxml", "Værger");
+        }
+
 
     public void goBack(ActionEvent actionEvent) {
     }
@@ -32,7 +37,7 @@ public class AdminChildListController {
     }
 
 
-   /* public void initialize() {
+   /*public void initialize() {
         TableColumn id = new TableColumn("ID");
         TableColumn firstName = new TableColumn("firstName");
         TableColumn lastName = new TableColumn("lastName");
