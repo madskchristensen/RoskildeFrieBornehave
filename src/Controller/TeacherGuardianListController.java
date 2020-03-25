98f0823a52d7705eb9c12a62126ea0369f6743b5
Rasmussejer <rasm897c@stud.kea.dk>
2020-03-25 18:26:50 +0100

    tableview added guardian

--- src/Controller/TeacherGuardianListController.java
+++ src/Controller/TeacherGuardianListController.java
@@ -1,14 +1,46 @@
 package Controller;
 
 import Business.Main;
+import Business.MemberRepository;
+import Business.TableManager;
+import Business.TeacherRepository;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class TeacherGuardianListController {
+public class TeacherGuardianListController implements Initializable {
+
+    public GridPane gridPane;
     public Button backButton;
     public Button childButton;
+    private TableManager tableManager;
+    private MemberRepository teacherRepo;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            tableManager = new TableManager();
+            //pass the Column names and column properties to the createTable method
+            String[] colNavn = new String[]{"ID", "Fornavn", "Efternavn", "Adresse", "Telefon Nummer", "E-mail"};
+            //the column Properties (colProp) are the names of the class attributes you want to read
+            String[] colProp = new String[]{"id", "firstName", "lastName", "address", "phoneNumber", "email"};
+            //the column name and property arrays must run in the same order
+            teacherRepo = new TeacherRepository("administrator", "admin_pass");
+            //initialize the table
+            TableView table = tableManager.createTable(colNavn, colProp, teacherRepo.getAllMembers());
+            //add table to fxml
+            gridPane.add(table, 0, 0);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void handleGoBack(ActionEvent actionEvent) throws IOException {
         Main.sceneManager.getPreviousScene();
